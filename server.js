c0efcbe5acc9c1f46fdafa404ece2f8d148356d3
elias-291356 <ikucher.002@gmail.com>
2023-12-07 13:34:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from './app.js';
-// r1ceFS4e22JYqSe1
-const DB_HOST = 'mongodb+srv://Illia:r1ceFS4e22JYqSe1@cluster0.dqo4lam.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+const { DB_HOST } = process.env;
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {

4897940647449467812ad497f01baeae7e2bc4e8
elias-291356 <ikucher.002@gmail.com>
2023-12-07 18:25:20 +0400

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,9 +1,20 @@
 import { Schema, model } from "mongoose";
 
 const contactsSchema = new Schema({
-  name: String,
-  email: String,
-  phone: Number,
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
 
 })
 

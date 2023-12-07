daac8403d403090d502c9fb127d128c4e306c9e5
elias-291356 <ikucher.002@gmail.com>
2023-12-07 14:03:21 +0400

    to yarn

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-import mongoose from "mongoose";
-import { app } from './app.js';
+import app from './app.js';
 
-const { DB_HOST } = process.env;
-mongoose.connect(DB_HOST)
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000")
-    })
 
-  })
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
-  })
-
+const { DB_HOST } = process.env
+app.listen(3002, () => {
+  console.log("Server running. Use our API on port: 3002")
+})
